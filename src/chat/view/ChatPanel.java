c285e30c44ad94a4e87d57f70b5d18a90a14f94a
BraisleCoanel <31518773+BraisleCoanel@users.noreply.github.com>
2017-11-13 13:51:31 -0700

    Created ChatPanel class and ChatFrame class.

---
+++ src/chat/view/ChatPanel.java
@@ -0,0 +1,15 @@
+package chat.view;
+
+import javax.swing.JPanel;
+import chat.controller.ChatbotController;
+
+
+public class ChatPanel extends JPanel
+{
+	private ChatbotController appController;
+	
+	public ChatPanel(ChatbotController appController)
+	{
+		super();	
+	}
+}
